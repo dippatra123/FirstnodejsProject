24eb1ce995ccfab203f4facc412af8e618a06beb
Dip Patra <patradip6@gmail.com>
2026-05-15 10:52:52 +0530

    some change in db.js

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
-// db.js
 const mysql = require("mysql2/promise");
 
-// Create and export a connection pool
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "DPsep@2020", // 🛠️ replace with your real MySQL password
-  database: "emp_database", // 🛠️ replace with your DB name
+  password: "DPsep@2020",
+  database: "emp_database",
 });
 
 module.exports = db;
